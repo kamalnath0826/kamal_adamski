2493e69a0e13f2daace455f6f020f5d003df01ce
kamal singh <mr.kamalnath@gmail.com>
2017-04-19 15:58:08 +0200

    Added closed loop controller.

--- src/turtle_controller.cpp
+++ src/turtle_controller.cpp
@@ -1,5 +1,18 @@
 #include <ros/ros.h>
+#include <turtlesim/Pose.h>
 #include <geometry_msgs/Twist.h>
+
+double goal_position=1.0;
+double forward_control=0.0;
+double k_p = 1.0;
+
+void posecallback(const turtlesim::Pose::ConstPtr&pose_msg)
+{
+ double x_error = goal_position - pose_msg->x;
+ forward_control= k_p* x_error;
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -7,14 +20,16 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
 
   ros::Publisher control_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",10);
-
+  ros::Subscriber pose_sub = nh.subscribe("/turtle1/pose", 10, &posecallback);
   ros::Rate rate(100.0);
 
   while (ros::ok())
   {
+    ros::spinOnce();
+
     geometry_msgs::Twist controls;
-    controls.linear.x = 5.0;
-    controls.angular.z = 5.0;
+    controls.linear.x = forward_control;
+    controls.angular.z = 0.0;
 
     control_pub.publish(controls);
 
